43a2de2e220ed922b28e0dade37cdccc3b9cea81
aromastar <aromastar.spb@gmail.com>
2022-07-30 22:09:14 +0300

    Add lazy load images

--- src/ts/lazy-load.ts
+++ src/ts/lazy-load.ts
@@ -1,30 +1,34 @@
-// const isIntersectionObserverSupported = (): boolean =>
-//   typeof IntersectionObserver !== 'undefined'
-// const lazyLoadImages: NodeListOf<HTMLImageElement> = document.querySelectorAll('.lazy-load-img')
+const lazyLoadImages: NodeListOf<HTMLImageElement> = document.querySelectorAll('.lazy-load-image')
+const IMAGE_LOADED_CLASSNAME: string = 'image-loaded'
+const IMAGE_SRC_ATTR: string = 'data-lazy-src'
+const IMAGE_TRANSITION_DURATION: number = 500
 
+if ('IntersectionObserver' in window) {
+  function onImageLoad(this: HTMLImageElement) {
+    const blurredImage: HTMLElement = this.nextElementSibling as HTMLImageElement
+    blurredImage.classList.add(IMAGE_LOADED_CLASSNAME)
+    setTimeout(() => {
+      blurredImage.remove()
+    }, IMAGE_TRANSITION_DURATION)
+    this.removeEventListener('load', onImageLoad)
+  }
 
-// const observer = new IntersectionObserver((entries, observer) => {
-//   entries.forEach(({ target, isIntersecting }) => {
-//     console.log(target)
-//     if (isIntersecting) {
-//       const image = target.querySelector('.image--original') as HTMLImageElement
-//       const imageBlurred = target.querySelector('.image--blurred') as HTMLImageElement
-//       console.log(image)
+  const imageObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(({ target, isIntersecting }) => {
+      if (isIntersecting) {
+        target.setAttribute('src', target.getAttribute(IMAGE_SRC_ATTR) as string)
+        target.addEventListener('load', onImageLoad)
+        observer.unobserve(target)
+      }
+    })
+  }, {})
 
-//       image.setAttribute('src', target.getAttribute('data-src') as string);
-//       image.addEventListener('load', () => {
-//         image.classList.remove('is-loading')
-//         setTimeout(imageBlurred.remove, 300)
-//       })
-
-//       observer.unobserve(target);
-//   }
-//   })
-// }, {
-//   rootMargin: '0px',
-//   threshold: 1.0
-// })
-
-// lazyLoadImages.forEach(lazyImage => {
-//   observer.observe(lazyImage)
-// })
+  lazyLoadImages.forEach(lazyLoadImage => {
+    imageObserver.observe(lazyLoadImage)
+  })
+} else {
+  lazyLoadImages.forEach(lazyLoadImage => {
+    lazyLoadImage.setAttribute('src', lazyLoadImage.getAttribute(IMAGE_SRC_ATTR) as string);
+    (lazyLoadImage.nextElementSibling as HTMLImageElement).remove()
+  })
+}
